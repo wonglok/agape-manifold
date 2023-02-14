9aa0d57904295f1ed02f6aaea357f87f6b2b7d42
Wong Lok <wonglok@users.noreply.github.com>
2023-02-14 16:30:09 +0800

    save

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,13 @@ import dynamic from 'next/dynamic'
 import Instructions from '@/components/dom/Instructions'
 import { PLYLoader } from 'three-stdlib'
 import { hookWow } from '@/store/store'
-import nProgress from 'nprogress'
-import { MeshBasicMaterial, Object3D, Points } from 'three'
+import nProgress, { done } from 'nprogress'
+import { BufferAttribute, BufferGeometry, MeshBasicMaterial, Object3D, Points } from 'three'
 import { OrbitControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import { useEffect } from 'react'
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter'
+import { PLYExporter } from 'three/examples/jsm/exporters/PLYExporter'
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
@@ -16,52 +19,148 @@ const Logo = dynamic(() => import('@/components/canvas/Logo'), { ssr: false })
 export default function Page(props) {
   let ply = hookWow((s) => s.ply)
 
+  let getNewPLY = (plyGeo) => {
+    /** @type {BufferGeometry} */
+    let cloned = plyGeo
+
+    let arrPos = []
+    let arrCol = []
+
+    let count = cloned.attributes.position.count
+
+    for (let i = 0; i < count; i++) {
+      //
+      if (i % 3 === 0) {
+        arrPos.push(
+          //
+          cloned.attributes.position.getX(i),
+          cloned.attributes.position.getY(i),
+          cloned.attributes.position.getZ(i),
+        )
+        arrCol.push(
+          //
+          cloned.attributes.color.getX(i),
+          cloned.attributes.color.getY(i),
+          cloned.attributes.color.getZ(i),
+        )
+      }
+      //
+    }
+
+    let buff = new BufferGeometry()
+    buff.setAttribute('position', new BufferAttribute(new Float32Array(arrPos), 3))
+    buff.setAttribute('color', new BufferAttribute(new Float32Array(arrCol), 3))
+
+    return buff
+  }
+
   // let progress = useWow((s) => s.progress)
   return (
     <>
       {(!ply || true) && (
         <Instructions>
-          {
-            <button
-              className='p-3 px-5 text-white bg-blue-500 rounded-lg'
-              onClick={() => {
-                //
-                let input = document.createElement('input')
-                input.type = 'file'
-                input.onchange = (ev) => {
-                  let first = ev.target?.files[0]
+          <button
+            className='p-3 px-5 mx-3 text-white bg-blue-500 rounded-lg'
+            onClick={() => {
+              //
+              let input = document.createElement('input')
+              input.type = 'file'
+              input.onchange = (ev) => {
+                let first = ev.target?.files[0]
 
-                  if (first) {
-                    let url = URL.createObjectURL(first)
-                    nProgress.start()
-                    let plyLoader = new PLYLoader()
-                    plyLoader
-                      .loadAsync(`${url}`, (ev) => {
-                        nProgress.set((ev.loaded / ev.total) * 0.8)
-                        // useWow.setState({ progress: ev.loaded / ev.total })
+                if (first) {
+                  let url = URL.createObjectURL(first)
+                  nProgress.start()
+                  let plyLoader = new PLYLoader()
+                  plyLoader
+                    .loadAsync(`${url}`, (ev) => {
+                      nProgress.set((ev.loaded / ev.total) * 0.8)
+                      // useWow.setState({ progress: ev.loaded / ev.total })
+                    })
+                    .then((ply) => {
+                      //
+
+                      ply = getNewPLY(ply)
+                      let o3d = new Object3D()
+
+                      let meshBasic = new MeshBasicMaterial({
+                        vertexColors: true,
                       })
-                      .then((ply) => {
-                        //
-                        let o3d = new Object3D()
 
-                        let meshBasic = new MeshBasicMaterial({
-                          vertexColors: true,
-                        })
+                      let mesh = new Points(ply, meshBasic)
+                      mesh.rotation.x = Math.PI * -0.5
+                      o3d.add(mesh)
+                      hookWow.setState({ ply: <primitive object={o3d}></primitive> })
 
-                        meshBasic.onBeforeCompile = (shader) => {
-                          shader.uniforms.time = { value: 0 }
-                          let works = () => {
-                            shader.uniforms.time.value = window.performance.now() / 1000
-                          }
+                      let exporter = new PLYExporter()
+                      exporter.parse(
+                        mesh,
+                        (binary) => {
+                          console.log(binary)
+                          let blo = new Blob([binary], { type: '' })
 
-                          let rAFID = 0
-                          let rAF = () => {
-                            rAFID = requestAnimationFrame(rAF)
-                            works()
-                          }
+                          let url2 = URL.createObjectURL(blo)
+
+                          let an = document.createElement('a')
+                          an.href = url2
+                          an.download = 'reduced.glb'
+                          an.click()
+
+                          nProgress.done()
+                        },
+                        // () => {},
+                        { binary: true, excludeAttributes: ['normal'] },
+                      )
+                    })
+                  //
+                }
+              }
+              input.click()
+            }}>
+            Reduce PLY File
+          </button>
+
+          <button
+            className='p-3 px-5 mx-3 text-white bg-blue-500 rounded-lg'
+            onClick={() => {
+              //
+              let input = document.createElement('input')
+              input.type = 'file'
+              input.onchange = (ev) => {
+                let first = ev.target?.files[0]
+
+                if (first) {
+                  let url = URL.createObjectURL(first)
+                  nProgress.start()
+                  let plyLoader = new PLYLoader()
+                  plyLoader
+                    .loadAsync(`${url}`, (ev) => {
+                      nProgress.set((ev.loaded / ev.total) * 0.8)
+                      // useWow.setState({ progress: ev.loaded / ev.total })
+                    })
+                    .then((ply) => {
+                      //
+
+                      let o3d = new Object3D()
+
+                      let meshBasic = new MeshBasicMaterial({
+                        vertexColors: true,
+                      })
+
+                      meshBasic.onBeforeCompile = (shader) => {
+                        shader.uniforms.time = { value: 0 }
+                        let works = () => {
+                          shader.uniforms.time.value = window.performance.now() / 1000
+                        }
+
+                        let rAFID = 0
+                        let rAF = () => {
                           rAFID = requestAnimationFrame(rAF)
+                          works()
+                        }
+                        rAFID = requestAnimationFrame(rAF)
 
-                          shader.vertexShader = /* glsl */ `
+                        shader.vertexShader = /* glsl */ `
 #include <common>
 #include <uv_pars_vertex>
 #include <uv2_pars_vertex>
@@ -97,34 +196,30 @@ void main() {
 
   float dist = length(gl_Position.xyz - cameraPosition.xyz);
 
-  if (dist >= 4.0) {
-    dist = 4.0;
+  if (dist >= 10.0) {
+    dist = 10.0;
   }
 
-  gl_Position.y += sin(dist / 4.0 * 3.141592 * 2.0 + time) * 0.3;
+  gl_Position.y += sin(dist / 10.0 * 3.141592 * 2.0 + time) * 0.3;
   gl_PointSize = ${(devicePixelRatio || 1.0).toFixed(1)};
 }
 
 `
-                        }
+                      }
 
-                        let mesh = new Points(ply, meshBasic)
-                        mesh.rotation.x = Math.PI * -0.5
-                        o3d.add(mesh)
-                        hookWow.setState({ ply: <primitive object={o3d}></primitive> })
-
-                        setTimeout(() => {
-                          nProgress.done()
-                        }, 1000)
-                      })
-                    //
-                  }
+                      let mesh = new Points(ply, meshBasic)
+                      mesh.rotation.x = Math.PI * -0.5
+                      o3d.add(mesh)
+                      hookWow.setState({ ply: <primitive object={o3d}></primitive> })
+                      nProgress.done()
+                    })
+                  //
                 }
-                input.click()
-              }}>
-              Select PLY File
-            </button>
-          }
+              }
+              input.click()
+            }}>
+            Run PLY File
+          </button>
         </Instructions>
       )}
     </>
